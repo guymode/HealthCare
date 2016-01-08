5f79d7ca61ff6098e58e0573cf6650c22155b84a
HoyeonKim <kimhoyun77@kut.ac.kr>
2016-01-08 10:52:48 +0900

    0108 google map 연동

---
+++ app/src/main/java/pl/polidea/asl/healthcare/googleMapActivity.java
@@ -0,0 +1,35 @@
+package pl.polidea.asl.healthcare;
+
+/**
+ * Created by IPSL on 2016-01-08.
+ */
+import android.app.Activity;
+import android.os.Bundle;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+public class googleMapActivity extends Activity {
+
+    static final LatLng SEOUL = new LatLng( 37.56, 126.97);
+    private GoogleMap map;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_googlemap);
+        map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
+                .getMap();
+        Marker seoul = map.addMarker(new MarkerOptions().position(SEOUL)
+                .title("Seoul"));
+
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom( SEOUL, 15));
+
+        map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+    }
+
+}
